5a88319a71d288a1b6fb1986b219f9cd7889cd32
xander <1149277941@qq.com>
2018-08-29 22:01:38 +0800

    在创建数据库时用到的一些函数定义

---
+++ operate_db.py
@@ -0,0 +1,46 @@
+# This file is created for development
+# Use current file to insert, modify, delete all of data which I need
+# Just for development
+
+
+def insert_data(db):
+    from .models import User, Role, Article
+    # 角色
+    Anonymous_role = Role(name='Anonymous')  # 访客
+    Ordinary_role = Role(name='Ordinary')  # 普通用户
+    Admin_role = Role(name='Admin')  # 管理员
+
+    # 用户
+    Tony = User(username='Tony', role=Admin_role, password='123')
+    zz = User(username='Guest', role=Anonymous_role, password='123')
+    Bob = User(username='Bob', role=Ordinary_role, password='123')
+
+    # 文章
+    # Essay = Article(user=Tony, title='Test article', classification='Test')
+
+    # 将记录添加到会话
+    db.session.add(Admin_role)
+    db.session.add(Anonymous_role)
+    db.session.add(Ordinary_role)
+
+    db.session.add(Tony)
+    db.session.add(zz)
+    db.session.add(Bob)
+
+    # db.session.add(Essay)
+
+    # 提交
+    db.session.commit()
+
+
+def remove_data(db):
+    '''remove all of data'''
+    pass
+
+
+def indite_article(db, user, title, classification=''):
+    pass
+
+
+
+
