b7cf4d6d022ec49661cf85cefed4bac3ce85f150
xander <1149277941@qq.com>
2018-08-30 16:59:34 +0800

    修改了markdown转html的部分规则

--- route/general.py
+++ route/general.py
@@ -72,8 +72,11 @@ def my_md2html(md):
 
     allowed_tags = ['a', 'abbr', 'acronym', 'b', 'blockquote', 'code', 'em', 'i',
                     'li', 'ol', 'pre', 'strong', 'ul', 'h1', 'h2', 'h3', 'h4', 'h5',
-                    'p', 'table', 'thead', 'tbody', 'td', 'tr', 'th', 'hr'
+                    'p', 'table', 'thead', 'tbody', 'td', 'tr', 'th', 'hr', 'img'
                     ]
+    allowed_attrs = {'*': ['class', 'id', 'align'],
+                     'a': ['href'],
+                     'img': ['src', 'alt']}
     # 初步生成html
     t = markdown.markdown(md, output_format='html',
                           extensions=[
@@ -85,7 +88,7 @@ def my_md2html(md):
                               'sane_lists',
                           ])
     # 净化html
-    html = bleach.linkify(bleach.clean(t, tags=allowed_tags, strip=True))
+    html = bleach.linkify(bleach.clean(t, tags=allowed_tags, attributes=allowed_attrs, strip=True))
 
     return html
 
